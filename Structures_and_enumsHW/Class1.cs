8c298b6f06a558d7d8bfccf0ec75d05fdcd8b534
Daria <kukuruzadasha86@gmail.com>
2023-02-16 18:51:41 +0200

    add

---
+++ Structures_and_enumsHW/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structures_and_enumsHW
+{ 
+class GFG
+    {
+        static string decToHexa(int n)
+        {
+            char[] hexaDeciNum = new char[2];
+            int i = 0;
+            while (n != 0)
+            {
+                int temp = 0;
+                temp = n % 16;
+                if (temp < 10)
+                {
+                    hexaDeciNum[i] = (char)(temp + 48);
+                    i++;
+                }
+                else
+                {
+                    hexaDeciNum[i] = (char)(temp + 55);
+                    i++;
+                }
+                n = n / 16;
+            }
+            string hexCode = "";
+
+            if (i == 2)
+            {
+                hexCode += hexaDeciNum[0];
+                hexCode += hexaDeciNum[1];
+            }
+            else if (i == 1)
+            {
+                hexCode = "0";
+                hexCode += hexaDeciNum[0];
+            }
+            else if (i == 0)
+                hexCode = "00";
+            return hexCode;
+        }
+        public string convertRGBtoHex(int R, int G, int B)
+        {
+            if ((R >= 0 && R <= 255) &&
+                (G >= 0 && G <= 255) &&
+                (B >= 0 && B <= 255))
+            {
+                string hexCode = "#";
+                hexCode += decToHexa(R);
+                hexCode += decToHexa(G);
+                hexCode += decToHexa(B);
+
+                return hexCode;
+            }
+
+            // The hex color code doesn't exist
+            else
+                return "-1";
+        }
+
+        // Driver code
+       
+    }
+}
+
--- Structures_and_enumsHW/Program.cs
+++ Structures_and_enumsHW/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace Structures_and_enumsHW
 {
@@ -80,7 +81,7 @@ namespace Structures_and_enumsHW
             {
                 int num2 = num;
                 string UI = num2.ToString();
-                int[] array = new int [UI.Length-1];
+                int[] array = new int[UI.Length];
 
                 for (int i = 0; num != 0; i++)
                 {
@@ -88,7 +89,7 @@ namespace Structures_and_enumsHW
                     num /= 16;
                 }
 
-                string arr =  null;
+                string arr = null;
                 for (int i = 0; i < array.Length; i++)
                 {
                     if (array[i] == 10)
@@ -96,7 +97,7 @@ namespace Structures_and_enumsHW
                         arr += 'A';
                         continue;
                     }
-                    if(array[i] == 11)
+                    if (array[i] == 11)
                     {
                         arr += 'B';
                         continue;
@@ -129,7 +130,62 @@ namespace Structures_and_enumsHW
                 return new string(add);
             }
         }
+        struct RGB
+        {
+            static string decToHexa(int n)
+            {
+                char[] hexaDeciNum = new char[2];
+                int i = 0;
+                while (n != 0)
+                {
+                    int temp = 0;
+                    temp = n % 16;
+                    if (temp < 10)
+                    {
+                        hexaDeciNum[i] = (char)(temp + 48);
+                        i++;
+                    }
+                    else
+                    {
+                        hexaDeciNum[i] = (char)(temp + 55);
+                        i++;
+                    }
+                    n = n / 16;
+                }
+                string hexCode = "";
 
+                if (i == 2)
+                {
+                    hexCode += hexaDeciNum[0];
+                    hexCode += hexaDeciNum[1];
+                }
+                else if (i == 1)
+                {
+                    hexCode = "0";
+                    hexCode += hexaDeciNum[0];
+                }
+                else if (i == 0)
+                    hexCode = "00";
+                return hexCode;
+            }
+            public string convertRGBtoHex(int R, int G, int B)
+            {
+                if ((R >= 0 && R <= 255) &&
+                    (G >= 0 && G <= 255) &&
+                    (B >= 0 && B <= 255))
+                {
+                    string hexCode = "#";
+                    hexCode += decToHexa(R);
+                    hexCode += decToHexa(G);
+                    hexCode += decToHexa(B);
+
+                    return hexCode;
+                }
+
+                else
+                    return "-1";
+            }
+        }
         static void Main(string[] args)
         {
             //Vector vectorA = new Vector(3, 5, 23);
@@ -139,10 +195,17 @@ namespace Structures_and_enumsHW
             //Console.WriteLine(vectorA.Subtraction(vectorB));
             //Console.WriteLine(vectorA.MultiNum(4));
 
-            //ProgrammerCalculator programmer = new ProgrammerCalculator(11111);
+            //ProgrammerCalculator programmer = new ProgrammerCalculator(5675);
             //Console.WriteLine(programmer.BinarySystem());
             //Console.WriteLine(programmer.OctalSystem());
             //Console.WriteLine(programmer.HexadecimalSystem());
+
+            RGB gFG = new RGB();
+
+            int R = 255, G = 255, B = 255;
+            Console.Write(gFG.convertRGBtoHex(R, G, B) + "\n");
+
+
 
 
         }
